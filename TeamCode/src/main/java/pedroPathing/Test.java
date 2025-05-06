c1d2a8136754c31327be2c28c1a2c566f37f05e5
DSK-Droids <ftcdroids@gmail.com>
2025-05-06 17:11:19 +0200

    post tuning unfinished auto

---
+++ TeamCode/src/main/java/pedroPathing/Test.java
@@ -0,0 +1,80 @@
+package pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+
+
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robocol.Command;
+import com.pedropathing.commands.FollowPath;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+
+
+@Autonomous(name = "5+1 Specimen Auto With Sample Drop ")
+public class Test extends LinearOpMode {
+
+
+
+    private Follower follower;
+
+    /**
+     * Starting Position of our robot
+     */
+    private final Pose startPose = new Pose(10, 105, Math.toRadians(-90));
+
+    private final Pose scorePreloadPose = new Pose(19, 124, Math.toRadians(-45));
+
+    private Path scorePreload;
+
+
+    public void buildPaths() {
+
+        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePreloadPose)));
+        scorePreload.setLinearHeadingInterpolation(Math.toRadians(-90), Math.toRadians(-45));
+    }
+
+    public void runOpMode(){
+
+        buildPaths();
+
+
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
+        CommandScheduler.
+
+
+
+
+
+
+        telemetry.addData("Status: ", "Ready to start...");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            follower.update();
+
+
+        }
+
+    }
+}
